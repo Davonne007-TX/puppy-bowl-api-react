1d791ac3d93f667321e15a2a2799ba5cc82c0dac
Vonney <davonnejv@gmail.com>
2023-07-31 22:02:32 -0500

    able to add all players in the console but with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,23 @@ function App() {
   return (
     <>
     <h1>Puppy Bowl</h1>
+    <AllPlayers setSelectedPlayer={setSelectedPlayer} />
 
-    {/* <AllPlayers setSelectedPlayer={setSelectedPlayer} /> */}
-    </>
+    {selectedPlayer && (
+      <div className="puppy-container">
+        <h2>Puppy Contender</h2>
+        <p>Name: {selectedPlayer.name}</p>
+        <p>Breed: {selectedPlayer.breed}</p>
+        <p>Status: {selectedPlayer.status}</p>
+        <p>Image: {selectedPlayer.imageUrl}</p>
+        <p>Created At: {selectedPlayer.createdAt}</p>
+      </div>
+    )}
+   </>
   )
 }
 
 export default App
+
+
+
