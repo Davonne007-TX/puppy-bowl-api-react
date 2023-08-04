18cefce56be638d0a929c9f1c401a05208f3b6bd
Vonney <davonnejv@gmail.com>
2023-08-03 22:12:26 -0500

    updating notes for today

---
+++ src/components/classNotes.js
@@ -0,0 +1,46 @@
+/*
+
+- Lecture Notes : Thursday July 3rd
+
+    - The dom - interacting the with DOM 
+
+    - Front End 
+        - html, css, javascript, react
+
+    - Back End
+        - API
+        - code
+        - React
+        - Javascript
+        - Databases
+        - SQL
+        
+
+    - Full Stack - both Front End and Back End 
+
+    ******
+        
+    - Stranger Things Assignment:
+      Due: August 14th
+      Group Project
+
+    - watch video
+
+    - theres an API that has helpful information for the API
+      - how API should be formatted
+      - see all the areas of the API
+      - Registration portion of the API
+        - parts of the application where the user should see if they are logged in, authenticated
+        -  via JWT
+        - shows how to get those tokens 
+
+  Crud - 
+    - create - POST
+    - read - GET
+    - update - PUT
+    - delete - DELETE
+
+
+*/
+
+
