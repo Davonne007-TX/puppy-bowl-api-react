fd27f97adf294b9c7a3d6df86c24f9f768461cf6
Vonney <davonnejv@gmail.com>
2023-11-04 21:56:15 -0500

    editing size of dog images, and editing the puppies name

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,83 +1,81 @@
-import React, { useState, useEffect } from 'react';
-import SearchBar from './SearchBar'
-import { fetchAllPlayers, fetchTeams, deletePlayer } from "./API"
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import SearchBar from "./SearchBar";
+import { fetchAllPlayers, fetchTeams, deletePlayer } from "./API";
+import { Link } from "react-router-dom";
 
-export default function AllPlayers({  setSelectedPlayer }) {
+export default function AllPlayers({ setSelectedPlayer }) {
   const [players, setPlayers] = useState([]);
-  const[searchPlayers, setSearch] = useState("");
-  const [teams, setTeams] = useState([])
+  const [searchPlayers, setSearch] = useState("");
+  const [teams, setTeams] = useState([]);
 
-//Fetch All Players
-useEffect(() => {  
-  const fetchPlayers = async () => {
-    const playersData = await fetchAllPlayers();
-    setPlayers(playersData);
+  //Fetch All Players
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      const playersData = await fetchAllPlayers();
+      setPlayers(playersData);
 
-    const teamsData = await fetchTeams();
-    setTeams(teamsData);
-    
-  }
-  fetchPlayers();
-}, []);
+      const teamsData = await fetchTeams();
+      setTeams(teamsData);
+    };
+    fetchPlayers();
+  }, []);
 
-//Search Bar
-const handelSearchInput = (options) => {
-  setSearch(options)
-};
+  //Search Bar
+  const handelSearchInput = (options) => {
+    setSearch(options);
+  };
 
-const searchedPuppies = players.filter((player) => 
-player.name.toLowerCase().includes(searchPlayers.toLowerCase())) 
+  const searchedPuppies = players.filter((player) =>
+    player.name.toLowerCase().includes(searchPlayers.toLowerCase())
+  );
 
-//Delete Function
-const handleDelete = async (playerId) => {
-  try {
-    await deletePlayer(playerId);
-    const updatedPlayers = players.filter((player) => player.id != playerId)
-    setPlayers(updatedPlayers);
-
-  } catch (error) {
-    console.log("Error", error)
-  }
-}
+  //Delete Function
+  const handleDelete = async (playerId) => {
+    try {
+      await deletePlayer(playerId);
+      const updatedPlayers = players.filter((player) => player.id != playerId);
+      setPlayers(updatedPlayers);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
 
   return (
-    <>  
-    <section>
-      <h2>Our Puppy Competitors</h2>
-      <p className="searchDescription">Search for player below &#x2199;</p>
+    <>
+      <section>
+        <h2>Our Puppy Competitors</h2>
+        <p className="searchDescription">Search for player below &#x2199;</p>
 
-      <SearchBar value={searchPlayers} onChange={handelSearchInput} />
+        <SearchBar value={searchPlayers} onChange={handelSearchInput} />
 
-      {searchedPuppies.map((player) => {
+        {searchedPuppies.map((player) => {
+          return (
+            <div key={player.id} className="puppy-container">
+              <p className="puppiesName">{player.name}</p>
+              <img src={player.imageUrl} className="dogImages" />
+              <p>Breed: {player.breed}</p>
+              <p>Cohort Id: {player.cohortId}</p>
+              <p>Created At: {player.createdAt}</p>
+              <p>ID: {player.id}</p>
+              <p>Status {player.status}</p>
+              <p>Team Id: {player.teamId}</p>
+              <p>Updated At: {player.updatedAt}</p>
 
-      return (
-        <div key={player.id} className="puppy-container">
-          <p className="puppiesName">Name: {player.name}</p>
-          <img src={player.imageUrl} className="dogImages" />
-          <p>Breed: {player.breed}</p>
-          <p>Cohort Id: {player.cohortId}</p>
-          <p>Created At: {player.createdAt}</p>
-          <p>ID: {player.id}</p>
-          <p>Status {player.status}</p>
-          <p>Team Id: {player.teamId}</p>
-          <p>Updated At: {player.updatedAt}</p>
+              <Link to={`/player/${player.id}`} className="seeDetails">
+                See Details
+              </Link>
 
-        <Link to={`/player/${player.id}`}
-          className="seeDetails">
-          See Details</Link>
-
-        <button
-          type="button" 
-          className="deleteButton" 
-          onClick={() => handleDelete(player.id)}>
-          Delete Player</button>
-          </div>
-          )
-        })} 
-    </section>
+              <button
+                type="button"
+                className="deleteButton"
+                onClick={() => handleDelete(player.id)}
+              >
+                Delete Player
+              </button>
+            </div>
+          );
+        })}
+      </section>
     </>
-  )
+  );
 }
-
-
--- src/components/SinglePlayer.jsx
+++ src/components/SinglePlayer.jsx
@@ -1,37 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
-import { fetchPlayerById } from './API'
+import React, { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { fetchPlayerById } from "./API";
 
 export default function SinglePlayer({ setSelectedPlayer }) {
-  const [player, setPlayer] = useState({})
+  const [player, setPlayer] = useState({});
 
-  let { id } = useParams()
+  let { id } = useParams();
 
-  useEffect(() => {  
-     const fetchPlayer = async () => {
-       const {data: {player}} = await fetchPlayerById(id);
-       setPlayer(player);
-    }
-   fetchPlayer();
-  }, [])
+  useEffect(() => {
+    const fetchPlayer = async () => {
+      const {
+        data: { player },
+      } = await fetchPlayerById(id);
+      setPlayer(player);
+    };
+    fetchPlayer();
+  }, []);
 
   return (
     <div className="singlePuppy-container">
       <h2>You have Selected Puppy Contender &#x2199;</h2>
-        <p className="puppiesName">Name: {player.name}</p>
-        <img src={player.imageUrl} className="dogImages" />
-        <p>Breed: {player.breed}</p>
-        <p>Cohort Id: {player.cohortId}</p>
-        <p>ID: {player.id}</p>
-        <p>Team Id: {player.teamId}</p>
-        <p>Status {player.status}</p>
-        <Link to={"/allPlayers"} className="goBack">Back to List</Link>
-   </div>
-  )
+      <p className="puppiesName">{player.name}</p>
+      <img src={player.imageUrl} className="dogImages" />
+      <p>Breed: {player.breed}</p>
+      <p>Cohort Id: {player.cohortId}</p>
+      <p>ID: {player.id}</p>
+      <p>Team Id: {player.teamId}</p>
+      <p>Status {player.status}</p>
+      <Link to={"/allPlayers"} className="goBack">
+        Back to List
+      </Link>
+    </div>
+  );
 }
-
-
-
-
-
-
